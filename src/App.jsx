dd388320da2aed5bb32ae3f0013509ec1cda02af
Sven Theobald <sven@MacBook-Air-von-Sven.local>
2025-11-09 16:47:01 +0100

    Projektstruktur aufgesetzt

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <h1>Hallo</h1>
+    </>
+  )
+}
+
+export default App
